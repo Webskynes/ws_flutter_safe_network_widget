bcbdf4353e8e11be95e5027c368a61c08358083c
vipin9821 <50833659+Vipin9821@users.noreply.github.com>
2023-01-27 21:58:36 +0530

    [APP] update initial config

---
+++ lib/src/network_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class NetworkSafeWidge extends StatelessWidget {
+  const NetworkSafeWidge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('jkhbasdf'),);
+  }
+}
---
+++ lib/ws_flutter_safe_network_widget.dart
@@ -0,0 +1,9 @@
+library ws_flutter_safe_network_widget;
+
+export 'src/index.dart';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
